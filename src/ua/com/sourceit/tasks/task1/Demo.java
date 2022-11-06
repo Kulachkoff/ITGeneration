ed9f2bb496b379d0eeafce00250619605aabda70
Max Kravchenko <32498198+Kulachkoff@users.noreply.github.com>
2022-11-06 21:07:56 +0200

    Update Demo.java

--- src/ua/com/sourceit/tasks/task1/Demo.java
+++ src/ua/com/sourceit/tasks/task1/Demo.java
@@ -2,11 +2,11 @@ package ua.com.sourceit.tasks.task1;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println(SubTask1.sumOfTwo(5, 6));                    // Done
-        System.out.println(SubTask2.concatStrings("String1", "String2"));     // Done
-        System.out.println(SubTask3.greatestCommonDivisor(45, 67));     // Done
-        System.out.println(SubTask4.sumOfDigits(4518123));                 // Done
-        SubTask5.allPrimeNumbers();                                           // Done
-        SubTask6                                                              // TODO
+        System.out.println(SubTask1.sumOfTwo(5, 6));                            // Done
+        System.out.println(SubTask2.concatStrings("String1", "String2"));       // Done
+        System.out.println(SubTask3.greatestCommonDivisor(45, 67));             // Done
+        System.out.println(SubTask4.sumOfDigits(4518123));                      // Done
+        SubTask5.allPrimeNumbers();                                             // Done
+        SubTask6                                                                // TODO
     }
 }
